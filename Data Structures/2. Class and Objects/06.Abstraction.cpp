c18f6d2630d6db9b5b119b5da5c0bf2101b41d13
tayma-06 <sultanakhadiza37@gmail.com>
2025-10-29 04:29:14 +0600

    Initial Commit

--- Data Structures/2. Class and Objects/06.Abstraction.cpp
+++ Data Structures/2. Class and Objects/06.Abstraction.cpp
@@ -2,3 +2,28 @@
 #include<string>
 using namespace std;
 
+class Shape
+{
+public:
+    virtual void draw() = 0; // Pure virtual function
+};
+
+class Circle : public Shape
+{
+public:
+    void draw() override
+    {
+        cout << "Drawing a Circle" << endl;
+    }
+};
+
+int main()
+{
+    Shape* shape = new Circle();
+    shape->draw(); // Output: Drawing a Circle
+    delete shape;
+    Circle c;
+    c.draw(); // Output: Drawing a Circle
+    
+    return 0;
+}
