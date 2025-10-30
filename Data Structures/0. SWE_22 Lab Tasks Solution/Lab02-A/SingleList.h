4d0a076f61434efd138f13beae143705f12f3719
tayma-06 <sultanakhadiza37@gmail.com>
2025-10-31 03:49:34 +0600

    New Files

---
+++ Data Structures/0. SWE_22 Lab Tasks Solution/Lab02-A/SingleList.h
@@ -0,0 +1,27 @@
+// SingleList.h file
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+class List
+{
+    Node *head;
+    Node *tail;
+
+public:
+    List()
+    {
+        head = tail = NULL;
+    }
+};
---
+++ Data Structures/0. SWE_22 Lab Tasks Solution/Lab02-A/myStack.cpp
@@ -0,0 +1,15 @@
+// myStack.cpp file
+#include <iostream>
+#include <SingleList.h>
+using namespace std;
+
+class myStack
+{
+    List *stack = new List();
+};
+
+int main()
+{
+
+    return 0;
+}
