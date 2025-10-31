6550a742b033965cb68ff1562fa811ed4c6416f5
tayma-06 <sultanakhadiza37@gmail.com>
2025-11-01 02:21:09 +0600

    Updated

--- Data Structures/0. SWE_22 Lab Tasks Solution/Lab02-A/SingleList.h
+++ Data Structures/0. SWE_22 Lab Tasks Solution/Lab02-A/SingleList.h
@@ -24,4 +24,59 @@ public:
     {
         head = tail = NULL;
     }
+
+    void pushFront(int val)
+    {
+        Node *newNode = new Node(val);
+        if (head == NULL)
+        {
+            head = tail = newNode;
+            return;
+        }
+        else
+        {
+            newNode->next = head; // (*newNode).next = head;
+            head = newNode;
+        }
+    }
+
+    void popFront()
+    {
+        if (head == NULL)
+        {
+            cout << "No Data. Invalid Operation" << endl;
+            return;
+        }
+        else if (head == tail)
+        {
+            head = tail = NULL;
+            return;
+        }
+        Node *temp = head;
+        head = head->next; // moving the head to next element
+        temp->next = NULL; // disconnecting the first element from the list
+        delete temp;       // deleting the first element
+    }
+
+    int getHead()
+    {
+        if (head == NULL)
+        {
+            cout << "Stack is empty!" << endl;
+            return -1;
+        }
+        return head->data;
+    }
+
+    int size()
+    {
+        int size = 0;
+        Node *temp = head;
+        while (temp != NULL)
+        {
+            temp = temp->next;
+            size++;
+        }
+        return size;
+    }
 };
--- Data Structures/0. SWE_22 Lab Tasks Solution/Lab02-A/myStack.cpp
+++ Data Structures/0. SWE_22 Lab Tasks Solution/Lab02-A/myStack.cpp
@@ -1,15 +1,52 @@
 // myStack.cpp file
 #include <iostream>
-#include <SingleList.h>
+#include "SingleList.h"
 using namespace std;
 
 class myStack
 {
-    List *stack = new List();
+    List list;
+
+public:
+    void push(int val)
+    {
+        list.pushFront(val);
+    }
+
+    void pop()
+    {
+        list.popFront();
+    }
+
+    int top()
+    {
+        return list.getHead();
+    }
+
+    int size()
+    {
+        return list.size();
+    }
+
+    bool empty()
+    {
+        return list.size() == 0 ? true : false;
+    }
 };
 
 int main()
 {
+    myStack stack;
+    stack.push(4);
+    stack.push(3);
+    stack.push(2);
+
+    cout << "Data: " << endl;
+    while (!stack.empty())
+    {
+        cout << stack.top() << endl;
+        stack.pop();
+    }
 
     return 0;
 }
