d3f813ced61ffc6e3ccc07de518b67ec1e080700
tayma-06 <sultanakhadiza37@gmail.com>
2025-11-01 18:41:31 +0600

    Modified

--- Data Structures/4. Stack/02.EasyProblems/01.The Celebrity Problem/01.Naive Approach/solution.cpp
+++ Data Structures/4. Stack/02.EasyProblems/01.The Celebrity Problem/01.Naive Approach/solution.cpp
@@ -12,19 +12,21 @@ public:
     {
         int n = mat.size();
 
-        vector<int> indegree(n, 0), outdegree(n, 0);
-        for (int i = 0; i < n; i++)
+        vector<int> indegree(n, 0);  // how many people know person i
+        vector<int> outdegree(n, 0); // how many people person i knows
+
+        for (int i = 0; i < n; i++) // i is current person
         {
-            for (int j = 0; j < n; j++)
+            for (int j = 0; j < n; j++) // j is other people
             {
-                int x = mat[i][j];
-                outdegree[i] += x;
-                indegree[j] += x;
+                int x = mat[i][j]; // x=1 i knows j & x=0 i doesn't know j
+                outdegree[i] += x; // add if i knows the person
+                indegree[j] += x;  // add if j knows i
             }
         }
         for (int i = 0; i < n; i++)
         {
-            if (indegree[i] == n && outdegree[i] == 1)
+            if (indegree[i] == n && outdegree[i] == 1) // if i knows only ownself and everyone else knows i
             {
                 return i;
             }
