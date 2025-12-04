11a33fc181a7593f293c166616db788fdead13e7
tayma-06 <sultanakhadiza37@gmail.com>
2025-12-04 18:05:51 +0600

    New File And Modification

--- Data Structures/0. Class_Lectures/Queue&It'sApplication/02.QueueUsingCircularArray.cpp
+++ Data Structures/0. Class_Lectures/Queue&It'sApplication/02.QueueUsingCircularArray.cpp
@@ -1,19 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class myQueue
+class myCircularQueue
 {
     int *arr;
-    int size;
     int capacity;
+    int size;
+    int front;
 
 public:
-    myQueue(int cap)
+    myCircularQueue(int cap)
     {
         capacity = cap;
         arr = new int[capacity];
         size = 0;
+        front = 0;
     }
+
     void enqueue(int val)
     {
         if (size == capacity)
@@ -21,66 +24,63 @@ public:
             cout << "Stack Overflow" << endl;
             return;
         }
-        arr[size++] = val;
+        int rear = (front + size) % capacity;
+        arr[rear] = val;
+        size++;
     }
-    void dequeue()
+
+    int dequeue()
     {
         if (size == 0)
         {
             cout << "Stack Underflow" << endl;
-            return;
+            return -1;
         }
-        for (int i = 0; i < size; i++)
-        {
-            arr[i - 1] = arr[i];
-        }
+        int result = arr[front];
+        front = (front + 1) % capacity;
         size--;
+        return result;
     }
+
+    int sizeOf()
+    {
+        return size;
+    }
+
     int getFront()
     {
-        return arr[0];
+        if (sizeOf() == 0)
+        {
+            return -1;
+        }
+        return arr[front];
     }
     int getRear()
     {
-        return arr[size - 1];
+        if (sizeOf() == 0)
+        {
+            return -1;
+        }
+        return arr[(front + size) % capacity];
     }
     bool isEmpty()
     {
         return size == 0;
     }
-    bool isFull()
-    {
-        return size == capacity;
-    }
-    void display()
-    {
-        for (int i = 0; i < size; i++)
-        {
-            cout << arr[i] << " ";
-        }
-        cout << endl;
-    }
-    int sizeOf()
-    {
-        return size;
-    }
 };
 
 int main()
 {
-    myQueue q(5);
+    myCircularQueue q(5);
     q.enqueue(1);
     q.enqueue(2);
     q.enqueue(3);
     q.enqueue(4);
     q.enqueue(5);
 
-    q.display();
-
     q.enqueue(6);
 
     q.dequeue();
-    q.display();
 
     cout << "Front element: " << q.getFront() << endl;
     cout << "Rear element: " << q.getRear() << endl;
